701da9c8068875691b6a5e364b68ddb379066098
Ark <arkadiy@hypem.com>
2011-02-12 17:29:40 -0500

    Detect WITCH HOUSE

--- familiar.js
+++ familiar.js
@@ -1,5 +1,17 @@
 (function( $ ){
-  $.fn.myPlugin = function() {
+  $.fn.familiar = function(options) {
+	  var settings = {
+	  };
+
+	  var alphabet = "▓□ђ░ıℜЗ▇▲∆⊇≥❖Λ•✝ℑ◊■▅†✌∞Ʌ▼█≤ЭЏ⊆‡▂э△Δ";
+
+	  this.change( function(){
+		  var c = this.value.charAt(this.value.length - 1);
+
+		  if(alphabet.indexOf(c) > 0){
+			  console.log(c);
+		  }
+	  });
   };
 })( jQuery );
 
