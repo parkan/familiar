27c18185964adb8e64a88fa748086d69cd898a96
Ark <arkadiy@hypem.com>
2011-02-12 18:47:34 -0500

    Make nice rows of keys

--- js/familiar.js
+++ js/familiar.js
@@ -1,24 +1,34 @@
 (function( $ ){
   $.fn.familiar = function(options) {
-	  var settings = {
-	  };
 
-	  var alphabet = "▓□ђ░ıℜЗ▇▲∆⊇≥❖Λ•✝ℑ◊■▅†✌∞Ʌ▼█≤ЭЏ⊆‡▂э△Δ";
+	  var settings = {
+						alphabet : "▓□ђ░ıℜЗ▇▲∆⊇≥❖Λ•✝ℑ◊■▅†✌∞Ʌ▼█≤ЭЏ⊆‡▂э△Δ"
+	  };
+		if(options){
+						$.extend( settings, options );
+		}
 
 	  this.keyup( function(){
 		  var c = this.value.charAt(this.value.length - 1);
 
-		  if(alphabet.indexOf(c) > 0){
+		  if(settings.alphabet.indexOf(c) > 0){
 			  console.log(c);
 		  }
 	  });
 
 		// KEYBOARD
-		var layout = alphabet.split().join(' ');
+		var layout = [];
+		var rowsize = Math.floor(Math.sqrt(alphabet.length));
+		for(var i = 0; ((i+1) * rowsize) <= alphabet.length; i++){
+						row = settings.alphabet.substr(i * rowsize, rowsize);
+						row = row.split('').join(' ');
+						layout.push(row);
+		}
+
 		this.keyboard({
 						layout : 'custom',
 						customLayout : {
-										default : layout
+										'default' : layout
 						},
 						restrictInput : true
 		});
