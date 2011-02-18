f6a04dc17c4d6d641bd7a77bcb11d869ad044f46
Ark <arkadiy@hypem.com>
2011-02-17 23:08:24 -0500

    Remove keyup trigger

--- js/familiar.js
+++ js/familiar.js
@@ -50,18 +50,6 @@
 		var ev_show_keyboard = jQuery.extend(true, {}, this.data("events").focus[0]);
 		this.unbind('focus'); // do not show keyboard on focus as normal
 
-		// trigger keyboard display on detected WITCH HOUSE characters
-		/*
-		this.keyup( function(){
-			var c = this.value.charAt(this.value.length - 1);
-
-			if(settings.alphabet.indexOf(c) > 0){
-				console.log(c);
-				ev_show_keyboard.handler.apply(this);
-			}
-		});
-		*/
-
 		// trigger keyboard display on SHIFT while focused (Chrome 8/Safari 4/FF 3/IE)
 		$(document).keydown( function(event){
 			if(
